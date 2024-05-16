e38877bc84fc8679bd5e072f58f72870e14c69fa
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-05-16 17:56:42 +0800

    Created Seq2SeqModel file to bring together encoders and decoders. Pretraining model step

---
+++ Seq2SeqModel.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+"""
+    This class puts together the decoder and encoder and 
+    receives Klingon and Engish data from the tokenization process
+
+"""
+
+class Seq2SeqModel(nn.Module):
+    def __init__(self, encoder, decoder, trainer):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.trainer = trainer
+        # cause encoder and decoder must have same no.of layers
+        assert (
+            encoder.hidden_dim == decoder.hidden_dim
+        ), "Hidden dimensions of encoder and decoder must be equal!"
+        assert (
+            encoder.n_layers == decoder.n_layers
+        ), "Encoder and decoder must have equal number of layers!"
+
+    """
+        Parameters:
+        ----------
+        input : Tensor
+            Input tensor containing token indices (seq_len, batch_size)
+            Tokenized English Data
+        
+        trg : Tensor
+            Target tensor containing token indices (seq_len, batch_size)
+            This is what our tokenized Klingon Data
+        
+        teacher_forcing_ratio: double
+            the % of time I use ground-truths aka during training
+         
+        Returns:
+        -------
+        prediction : Tensor
+            Predicted output tensor from the GRU (seq_len, batch_size, output_dim)
+        
+        hidden : Tensor
+            Hidden state tensor from the GRU (n_layers, batch_size, hid_dim)
+    """
+    def forward(self,input, trg, teacher_forcing_ratio):
+        batch_size = trg.shape[1]
+        trg_length = trg.shape[0]
+        trg_size = self.decoder.output.dim
+        #storing decorder outputs
+        outputs = torch.zeros(trg_length,batch_size,trg_size).to(self.trainer)
+        #output of encoder used as input for decoder
+        hidden = self.encoder(input)
+        
+
