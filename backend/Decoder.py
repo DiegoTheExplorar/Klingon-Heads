2cc10807819e7595321b2d94c57ca7866b092de5
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-06-01 20:21:00 +0800

    Update Decoder.py

--- backend/Decoder.py
+++ backend/Decoder.py
@@ -19,15 +19,16 @@ class Decoder(nn.Module):
         Dropout probability for the dropout layer
     
     """
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout, attention):
         super().__init__()
-        self.hid_dim = hid_dim
+        #self.hid_dim = hid_dim
         self.output_dim = output_dim
-        self.n_layers = n_layers
+        #self.n_layers = n_layers
         self.embedding = nn.Embedding(output_dim, emb_dim)
         self.rnn = nn.GRU(emb_dim, hid_dim, n_layers, dropout=dropout)
         self.fc_out = nn.Linear(hid_dim, output_dim)
         self.dropout = nn.Dropout(dropout)
+        self.attention = attention
 
     """
         Forward propagation step of decoding
@@ -51,16 +52,12 @@ class Decoder(nn.Module):
             Hidden state tensor from the GRU (n_layers, batch_size, hid_dim)
     """
     
-    def forward(self, trg, hidden):
-        #unsure trg is 3D
-        trg = trg.unsqueeze(0)
-        #input is converted into embeddings and dropout probability is applied
-        embedded = self.dropout(self.embedding(trg))
-        #print("Embedded shape:", embedded.shape)
-        #GRU layer computes new context based on previous context
-        output, hidden = self.rnn(embedded, hidden)
-        #print("Output shape after RNN:", output.shape)
-        #predicts output from GRU
-        prediction = self.fc_out(output.squeeze(0))
-        #print("Output shape after fc_out:", output.shape)
-        return prediction, hidden
+    def forward(self, input, hidden, encoder_outputs):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        a = self.attention(hidden[-1], encoder_outputs).unsqueeze(1)
+        weighted = torch.bmm(a, encoder_outputs.permute(1, 0, 2)).permute(1, 0, 2)
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        output, hidden = self.rnn(rnn_input, hidden)
+        output = self.fc_out(torch.cat((output, weighted, embedded), dim=2))
+        return output.squeeze(0), hidden
