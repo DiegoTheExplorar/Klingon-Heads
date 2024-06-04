e8683c04656935e18ea0436d30b07abcc5fcd14f
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-06-05 02:39:28 +0800

    Add Svelte app and integrate Gradio component

---
+++ svelte-app/src/components/GradioComponent.svelte
@@ -0,0 +1,14 @@
+<script>
+    // Add any necessary JavaScript here
+  </script>
+  
+  <style>
+    iframe {
+      width: 100%;
+      height: 800px; 
+      border: none;
+    }
+  </style>
+  
+  <iframe src="https://d397be7984525d1e0b.gradio.live" title="Gradio Interface"></iframe>
+  
