78144cfaf3b79897cf9d095e2a7ac0bb01ac13f9
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-06-10 14:57:28 +0800

    Added Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Auth from './Auth';
-import HistoryPage from './HistoryPage';
-import LandingPage from './LandingPage';
-import PrivateRoute from './PrivateRoute';
+import HistoryPage from './HistoryPage'; // Import the HistoryPage
+import SignIn from './SignIn';
 import Translator from './Translator';
-
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/translator" element={
-            <PrivateRoute>
-              <Translator />
-            </PrivateRoute>
-          } />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/auth" element={<Auth />} /> {/* Route for Authentication */}
+          <Route exact path="/" element={<SignIn />} /> {/* Render SignIn as default */}
+          <Route path="/translator" element={<Translator />} />
+          <Route path="/history" element={<HistoryPage />} /> {/* Route for the History Page */}
         </Routes>
       </div>
     </Router>
